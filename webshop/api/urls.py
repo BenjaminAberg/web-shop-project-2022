c09338b01ba9c2d95c6b58ab886bdbf801b5e2be
BenjaminAberg <benjamin.aberg@pp.inet.fi>
2022-11-14 19:09:48 +0200

    added basic urls

--- webshop/api/urls.py
+++ webshop/api/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import main
+from .views import index, ViewProductsView, AddProductsView
 
 urlpatterns = [
-    path('', main),
+    path('', index),
+    path('addproducts', AddProductsView.as_view()),
+    path('viewproducts', ViewProductsView.as_view())
 ]
--- webshop/api/views.py
+++ webshop/api/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from rest_framework import generics
+from .serializers import ProductSerializer
+from .models import Product
 
 # Create your views here.
-def main(request):
-    return HttpResponse("Hello!")
+def index(request):
+    num_products = Product.objects.all().count()
+
+    context = {'num_products': num_products}
+
+    return render(request, 'index.html', context)
+
+class ViewProductsView(generics.ListAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+class AddProductsView(generics.CreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
