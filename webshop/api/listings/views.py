b5413bf998cba66b5271bdeafcce232fc60db5bd
BenjaminAberg <benjamin.aberg@pp.inet.fi>
2022-11-28 09:32:59 +0200

    added support for pagination

--- webshop/api/listings/views.py
+++ webshop/api/listings/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render, reverse
 from .serializers import ListingSerializer
 from rest_framework import generics
+from rest_framework.generics import GenericAPIView
 from rest_framework.pagination import PageNumberPagination
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.response import Response
 from django.views import View
 from .models import Listing
 from .forms import CreateListingForm
@@ -9,14 +14,15 @@ from django.http import HttpResponse, HttpResponseRedirect
 
 # Create your views here.
 
-# WIP IMPLEMENT PAGINATION!!
-
-class CardSetPagination(PageNumberPagination):
-    page_size = 2
+class ListingSetPagination(PageNumberPagination):
+    page_size = 10
     page_size_query_param = 'page_size'
-    max_page_size = 10
+    max_page_size = 100
 
 class AddListingView(View):
+    permission_classes = [IsAuthenticated, ]
+    authentication_classes = [JWTAuthentication, ]
+
     def get(self, request):
         form = CreateListingForm()
         return render(request, 'create_listing_form.html', {"form": form})
@@ -35,6 +41,37 @@ class AddListingView(View):
             return HttpResponseRedirect(reverse("viewlistings"))
 
         return render(request, 'create_listing_form.html', {'form': form})
+
+class ListingListPageAPI(GenericAPIView):
+    permission_classes = [IsAuthenticatedOrReadOnly, ]
+    authentication_classes = [TokenAuthentication, ]
+    pagination_class = ListingSetPagination
+
+    def get_queryset(self):
+        return Listing.objects.all()
+
+    def get(self, request):
+        
+        listing = Listing.objects.all()
+
+        page = self.paginate_queryset(listing)
+        if page:
+            # queryset is not empty
+            serializer = ListingSerializer(page, many=True)
+            data = serializer.data
+        else:
+            # queryset is empty
+            data = []
+        return self.get_paginated_response(data)
+
+    def post(self, request):
+
+        serializer = ListingSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors, status=400)
 
 def view_listings(request):
     listings = Listing.objects.all()
--- webshop/api/urls.py
+++ webshop/api/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 from .views import index
-from .listings.views import view_listings, AddListingView, delete_listing
+from .listings.views import view_listings, AddListingView, delete_listing, ListingListPageAPI
 from .accounts.views import RegisterUser
 from rest_framework_simplejwt.views import TokenObtainPairView
 
 urlpatterns = [
     path('', index),
-    path('listings', view_listings, name="viewlistings"),
+    #path('listings', view_listings, name="viewlistings"),
+    path('listings', ListingListPageAPI.as_view(), name="viewlistings"),
     path('addlisting', AddListingView.as_view(), name="addlisting"),
     path('deletelisting/<int:listing_id>', delete_listing, name="deletelisting"),
     path('signup/', RegisterUser.as_view()),
