65a615a205d3fd44bbe4071f89de116cbdf6a0c0
BenjaminAberg <benjamin.aberg@pp.inet.fi>
2022-12-21 15:15:36 +0200

    venv changes

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Programmering\Web Technologies\web-shop-project-2022-BenjaminAberg\webshop\venv
+set VIRTUAL_ENV=C:\Programmering\Web Technologies\web-shop-project-2022-BenjaminAberg\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
